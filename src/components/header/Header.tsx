3485412aae08273d15c930363ee7cf097774ff20
khanh nguyen <khanhjoi1@gmail.com>
2024-03-19 17:20:58 +0700

    remove search in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -133,22 +133,6 @@ const Header = () => {
       <NavbarContent justify="end">
         {!isLogin && (
           <>
-            <NavbarItem className="hidden md:flex">
-              <Input
-                classNames={{
-                  base: "hidden md:block max-w-full sm:max-w-[10rem] h-10",
-                  mainWrapper: "h-full",
-                  input: "text-small",
-                  inputWrapper:
-                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                }}
-                placeholder="Type to search..."
-                size="sm"
-                startContent={<IoIosSearch size={18} />}
-                type="search"
-              />
-            </NavbarItem>
-            <NavbarItem></NavbarItem>
             <NavbarItem className="flex">
               <Button
                 onClick={() => {
